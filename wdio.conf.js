3652735eeb005597b5fd99159588ca1c0bb4d04f
Dilpreet Johal <automationbro@gmail.com>
2020-06-30 22:00:51 -0400

    chai assertion

--- wdio.conf.js
+++ wdio.conf.js
@@ -16,7 +16,7 @@ exports.config = {
   // NPM script (see https://docs.npmjs.com/cli/run-script) then the current working
   // directory is where your package.json resides, so `wdio` will be called from there.
   //
-  specs: ['./test/specs/**/*.js'],
+  specs: ['./test/specs/**/chai.js'],
   // Patterns to exclude.
   exclude: [
     // 'path/to/excluded/files'
@@ -131,6 +131,7 @@ exports.config = {
   mochaOpts: {
     ui: 'bdd',
     timeout: 60000,
+    require: ['@babel/register'],
   },
   //
   // =====
