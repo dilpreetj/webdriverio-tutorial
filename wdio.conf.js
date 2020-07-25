046cf0c56258447cbac79597960e9c2471d46fea
automationbro <automationbro@gmail.com>
2020-07-25 13:51:14 -0400

    add output file format for junit reporter

--- wdio.conf.js
+++ wdio.conf.js
@@ -142,6 +142,9 @@ exports.config = {
       'junit',
       {
         outputDir: './report',
+        outputFileFormat: function (options) {
+          return `results-${new Date().getTime()}.xml`;
+        },
       },
     ],
   ],
